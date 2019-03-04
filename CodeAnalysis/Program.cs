765d1f46ac64611f9eb90777622897d6962b93c6
fred <domaine75@gmail.com>
2019-03-04 18:34:01 +0100

    Initial commit

---
+++ CodeAnalysis/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System;
+using Microsoft.CodeAnalysis;
+
+namespace CodeAnalysis
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var tree = CSharpSyntaxTree.ParseText(@"
+using System;
+
+Class Foo
+{
+  void Bar(int x)
+  {
+  }
+}
+");
+      //var mscorlib = new MetadataFileReference(typeof(object).Assembly.Location);
+
+      var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+
+      //var comp = CSharpCompilation.Create("Demo").AddSyntaxTrees(tree).AddReferences(mscorlib).WithOptions(options);
+      var comp = CSharpCompilation.Create("Demo").AddSyntaxTrees(tree).WithOptions(options);
+
+      var res = comp.Emit("Demo.dll");
+
+      Console.ReadKey();
+    }
+  }
+}
